494385463041b893f6f907701fc23cee0d5d89b0
Zac Willmington <zac.willmington@gmail.com>
2018-07-20 17:54:17 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+    resources :artisits, :only [:show] do 
+        resources :songs, only: [:show, :index]
+    end 
+    
   resources :artists
   resources :songs
 end
