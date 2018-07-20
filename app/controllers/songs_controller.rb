c0c7e7808164728b6a1f2f3720ee22cf3353d79b
Zac Willmington <zac.willmington@gmail.com>
2018-07-20 19:00:56 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,5 @@
 class SongsController < ApplicationController
   def index
-      binding.pry
        @artist = Artist.find_by(:id => params[:artist_id])
       if @artist
           @songs = @artist.songs
