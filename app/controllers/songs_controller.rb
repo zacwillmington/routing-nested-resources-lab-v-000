a7bc09e06760ad0b6d8772abda888781efc91181
Zac Willmington <zac.willmington@gmail.com>
2018-07-20 18:16:48 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
        @artist = Artist.find_by(:id => params[:artist_id])
-      if params[:artist_id] && @artist
+      if @artist
           @songs = @artist.songs
           
       else 
