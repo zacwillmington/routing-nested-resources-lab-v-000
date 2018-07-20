e02189ff7a28df34f7ae0332d02563f8fb259cf1
Zac Willmington <zac.willmington@gmail.com>
2018-07-20 19:00:24 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,6 +19,7 @@ class SongsController < ApplicationController
           
       else
           flash[:alert] = "WOHW now cowboy. looks like this song doesn't exsist."
+          binding.pry
           redirect_to artist_songs_path @artist
       end
   end
