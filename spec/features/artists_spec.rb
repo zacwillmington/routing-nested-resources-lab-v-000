f75caa40e7760fb0acf83d20dfdbdfb397476382
Zac Willmington <zac.willmington@gmail.com>
2018-07-20 19:10:31 +0000

    automatically backed up by learn

--- spec/features/artists_spec.rb
+++ spec/features/artists_spec.rb
@@ -1,7 +1,7 @@
 describe "artists", type: :feature do
 
   before do
-    Artist.destroy_all
+    # Artist.destroy_all
     Song.destroy_all
     @artist = Artist.create!(name: "Daft Punk")
     @song = @artist.songs.create!(title: "The Grid")
